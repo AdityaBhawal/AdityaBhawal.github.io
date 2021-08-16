7b5fc2c0770f661265736ddcd53a398afac125c6
AdityaBhawal <aditya.bhawal@gmail.com>
2021-08-15 22:07:07 -0700

    Convert to gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "AdityaBhawal",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-FH66B51K5S",
+      },
+    },
+  ],
+};
